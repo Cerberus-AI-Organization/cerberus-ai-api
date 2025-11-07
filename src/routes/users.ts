2ea9191fd405ac0fc09fd77eda5f232067839909
sobotat <tomas.sobota@gmail.com>
2025-11-07 14:41:11 +0100

    Added get user

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getUsers, addUser, updateUser, deleteUser } from '../controllers/userController';
+import {getUsers, addUser, updateUser, deleteUser, getUser} from '../controllers/userController';
 import { authenticateToken } from '../middleware/authMiddleware';
 
 const router = Router();
@@ -7,6 +7,7 @@ const router = Router();
 router.use(authenticateToken);
 
 router.get('/', getUsers);
+router.get('/:id', getUser);
 router.post('/', addUser);
 router.put('/:id', updateUser);
 router.delete('/:id', deleteUser);

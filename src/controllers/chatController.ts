85d65ed03992502037c32a452d30e6660284ae12
sobotat <tomas.sobota@gmail.com>
2025-11-07 14:38:58 +0100

    Formated chatController.ts and modified postMesssage to send message, aichunks and aimessage

--- src/controllers/chatController.ts
+++ src/controllers/chatController.ts
@@ -1,139 +1,177 @@
-import { Request, Response } from "express";
-import { pool } from "../core/database";
+import {Request, Response} from "express";
+import {pool} from "../core/database";
 import {checkOnline} from "./computeNodeController";
 import {getNodeById} from "./ollamaController";
 import {runOllamaStream} from "../core/ollama";
+import {Message} from "../types/message";
 
 export const createChat = async (req: Request, res: Response) => {
-    try {
-        const { title } = req.body;
-        const user = (req as any).user;
+  try {
+    const {title} = req.body;
+    const user = (req as any).user;
 
-        const chatResult = await pool.query(
-            `INSERT INTO chats (title, created_by) VALUES ($1, $2) RETURNING *`,
-            [title, user.id]
-        );
+    const chatResult = await pool.query(
+      `INSERT INTO chats (title, created_by)
+       VALUES ($1, $2)
+       RETURNING *`,
+      [title, user.id]
+    );
 
-        const chat = chatResult.rows[0];
+    const chat = chatResult.rows[0];
 
-        await pool.query(
-            `INSERT INTO chat_users (chat_id, user_id) VALUES ($1, $2)`,
-            [chat.id, user.id]
-        );
+    await pool.query(
+      `INSERT INTO chat_users (chat_id, user_id)
+       VALUES ($1, $2)`,
+      [chat.id, user.id]
+    );
 
-        res.json(chat);
-    } catch (err: any) {
-        res.status(500).json({ error: err.message });
-    }
+    res.json(chat);
+  } catch (err: any) {
+    res.status(500).json({error: err.message});
+  }
 };
 
 export const getUserChats = async (req: Request, res: Response) => {
-    try {
-        const user = (req as any).user;
-        const result = await pool.query(
-            `SELECT c.* FROM chats c
-       JOIN chat_users cu ON c.id = cu.chat_id
+  try {
+    const user = (req as any).user;
+    const result = await pool.query(
+      `SELECT c.*
+       FROM chats c
+                JOIN chat_users cu ON c.id = cu.chat_id
        WHERE cu.user_id = $1
        ORDER BY c.last_modified DESC`,
-            [user.id]
-        );
-        res.json(result.rows);
-    } catch (err: any) {
-        res.status(500).json({ error: err.message });
-    }
+      [user.id]
+    );
+    res.json(result.rows);
+  } catch (err: any) {
+    res.status(500).json({error: err.message});
+  }
 };
 
 export const getChatMessages = async (req: Request, res: Response) => {
-    try {
-        const { id } = req.params;
-        const user = (req as any).user;
+  try {
+    const {id} = req.params;
+    const user = (req as any).user;
 
-        const resultChatUsers = await pool.query(`SELECT * FROM chat_users WHERE chat_id = $1 AND user_id = $2`,
-            [Number(id), user.id]);
-        if (resultChatUsers.rows.length === 0 || (resultChatUsers.rows.length === 0 && user.role !== 'admin')) {
-            return res.status(403).json({ message: 'You are not authorized to send messages in this chat' });
-        }
-
-        const result = await pool.query(
-            `SELECT * FROM messages WHERE chat_id = $1 ORDER BY created_at ASC`,
-            [id]
-        );
-        res.json(result.rows);
-    } catch (err: any) {
-        res.status(500).json({ error: err.message });
+    const resultChatUsers = await pool.query(`SELECT *
+                                              FROM chat_users
+                                              WHERE chat_id = $1
+                                                AND user_id = $2`,
+      [Number(id), user.id]);
+    if (resultChatUsers.rows.length === 0 || (resultChatUsers.rows.length === 0 && user.role !== 'admin')) {
+      return res.status(403).json({message: 'You are not authorized to send messages in this chat'});
     }
+
+    const result = await pool.query(
+      `SELECT *
+       FROM messages
+       WHERE chat_id = $1
+       ORDER BY created_at ASC`,
+      [id]
+    );
+    res.json(result.rows);
+  } catch (err: any) {
+    res.status(500).json({error: err.message});
+  }
 };
 
 export const postChatMessage = async (req: Request, res: Response) => {
-    try {
-        const { id } = req.params;
-        const { node_id, model, content } = req.body;
-        const user = (req as any).user;
+  try {
+    const {id} = req.params;
+    const {node_id, model, content} = req.body;
+    const user = (req as any).user;
 
-        const node = await getNodeById(Number(node_id));
-        const status = await checkOnline(node.ip, node.port);
-        if (status === 'offline') {
-            return res.status(500).json({ error: "Node is offline" });
-        }
-
-        const result = await pool.query(`SELECT * FROM chat_users WHERE chat_id = $1 AND user_id = $2`,
-            [id, user.id]);
-        if (result.rows.length === 0) {
-            return res.status(403).json({ message: 'You are not authorized to send messages in this chat' });
-        }
-
-        await pool.query(
-            `INSERT INTO messages (chat_id, sender_type, sender_id, content) VALUES ($1, 'user', $2, $3) RETURNING *`,
-            [id, user.id, content]
-        );
-
-        const session = `${id}`
-        let generatedMessage = "";
-        for await (const chunk of runOllamaStream(Number(node_id), model, session, [
-            { role: "system", content: "Your name is CerberusAI and you must help people in cybersecurity." },
-            { role: "user", content: content }
-        ])) {
-            generatedMessage += chunk.content;
-            res.write(JSON.stringify(chunk) + "\n");
-        }
-
-        await pool.query(
-            `INSERT INTO messages (chat_id, sender_type, sender_id, content) VALUES ($1, 'ai', $2, $3) RETURNING *`,
-            [id, null, generatedMessage]
-        );
-
-        res.end();
-    } catch (err: any) {
-        res.status(500).json({ error: err.message });
+    const node = await getNodeById(Number(node_id));
+    const status = await checkOnline(node.ip, node.port);
+    if (status === 'offline') {
+      return res.status(500).json({error: "Node is offline"});
     }
+
+    const result = await pool.query(`SELECT *
+                                     FROM chat_users
+                                     WHERE chat_id = $1
+                                       AND user_id = $2`,
+      [id, user.id]);
+    if (result.rows.length === 0) {
+      return res.status(403).json({message: 'You are not authorized to send messages in this chat'});
+    }
+
+    res.setHeader("Content-Type", "application/x-ndjson");
+    res.setHeader("Transfer-Encoding", "chunked");
+
+    const messageResult = await pool.query(
+      `INSERT INTO messages (chat_id, sender_type, sender_id, content)
+       VALUES ($1, 'user', $2, $3)
+       RETURNING *`,
+      [id, user.id, content]
+    );
+    const message: Message = messageResult.rows[0];
+    res.write(JSON.stringify({message: message}) + "\n");
+
+    const session = `${id}`
+    let generatedMessageContent = "";
+
+    for await (const chunk of runOllamaStream(Number(node_id), model, session, [
+      {role: "system", content: "Your name is CerberusAI and you must help people in cybersecurity."},
+      {role: "user", content: content}
+    ])) {
+      generatedMessageContent += chunk.content;
+      res.write(JSON.stringify({generated_chunk: chunk}) + "\n");
+    }
+
+    if (generatedMessageContent.trim() === "") {
+      generatedMessageContent = "Failed to generate message content. Please try again later.";
+    }
+
+    const generatedResult = await pool.query(
+      `INSERT INTO messages (chat_id, sender_type, sender_id, content)
+       VALUES ($1, 'ai', $2, $3)
+       RETURNING *`,
+      [id, null, generatedMessageContent]
+    );
+    const generatedMessage: Message = generatedResult.rows[0];
+    res.write(JSON.stringify({generated_message: generatedMessage}) + "\n");
+
+    if (generatedMessage.content.trim() === "") {
+      res.status(500).write(JSON.stringify({error: "Failed to generate message content. Please try again later."}) + "\n");
+      return;
+    }
+    res.end();
+  } catch (err: any) {
+    res.status(500).write(JSON.stringify({error: err.message}) + "\n");
+  }
 }
 
 export const deleteChat = async (req: Request, res: Response) => {
-    try {
-        const { id } = req.params;
-        const user = (req as any).user;
+  try {
+    const {id} = req.params;
+    const user = (req as any).user;
 
-        const result = await pool.query(
-            `SELECT * FROM chats WHERE id = $1`,
-            [id]
-        );
-        const chat = result.rows[0];
+    const result = await pool.query(
+      `SELECT *
+       FROM chats
+       WHERE id = $1`,
+      [id]
+    );
+    const chat = result.rows[0];
 
-        if (!chat) {
-            return res.status(404).json({ message: 'Chat not found' });
-        }
-
-        if (chat.created_by !== user.id || chat.created_by !== user.id && user.role !== 'admin') {
-            return res.status(403).json({ message: 'You are not authorized to delete this chat' });
-        }
-
-        const resultDelete = await pool.query(
-            `DELETE FROM chats WHERE id = $1`,
-            [id]
-        );
-
-        res.json({ message: 'Chat deleted successfully' });
-    } catch (err: any) {
-        res.status(500).json({ error: err.message });
+    if (!chat) {
+      return res.status(404).json({message: 'Chat not found'});
     }
+
+    if (chat.created_by !== user.id || chat.created_by !== user.id && user.role !== 'admin') {
+      return res.status(403).json({message: 'You are not authorized to delete this chat'});
+    }
+
+    const resultDelete = await pool.query(
+      `DELETE
+       FROM chats
+       WHERE id = $1`,
+      [id]
+    );
+
+    res.json({message: 'Chat deleted successfully'});
+  } catch (err: any) {
+    res.status(500).json({error: err.message});
+  }
 }
